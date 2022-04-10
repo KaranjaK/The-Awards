63707881a16e7ed0290f1bd6758fb6f660d64476
KK <karanjakelvin77@gmail.com>
2022-04-10 06:49:21 +0300

    It will add the project model

--- the_awards/models.py
+++ the_awards/models.py
@@ -1,3 +1,9 @@
+from turtle import title
 from django.db import models
 
 # Create your models here.
+
+# This is the Projects model
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    
