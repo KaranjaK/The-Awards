1e8106d715690617c29a06b9148a426a890a73f0
KK <karanjakelvin77@gmail.com>
2022-04-10 07:25:02 +0300

    It will create a test for testing the projects class

--- the_awards/tests.py
+++ the_awards/tests.py
@@ -1,3 +1,17 @@
+from email.mime import image
+from turtle import title
 from django.test import TestCase
+from .models import Projects
 
 # Create your tests here.
+# Testing for the project class
+class ProjectsTest(TestCase):
+
+    # Creating an instance of the Projects class for testing
+    def setUp(self):
+        self.project = Projects('Django unchained','image/yes.jpg','A project beyond normal','http://unchained.django.com')
+        self.project.save_project
+    
+    # Testing the instance created
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Projects))
