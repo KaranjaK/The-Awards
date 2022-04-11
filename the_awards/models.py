d9655c7ee7cfdd84d3caca9c2a94f9c34596d4b9
KK <karanjakelvin77@gmail.com>
2022-04-11 17:09:32 +0300

    It will restructure the models

--- the_awards/models.py
+++ the_awards/models.py
@@ -1,20 +1,31 @@
 from pyexpat import model
-from turtle import title
+from tkinter import CASCADE
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.forms import CharField
+import datetime as dt
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# User Profile Model
+class Profile(models.Model):
+    id = models.CharField(primary_key=True, max_length=20)
+    owner = models.OneToOneField(User, on_delete=CASCADE, related_name='profile')
+    image = CloudinaryField('image')
+    bio = models.CharField(max_length=40)
+    projects = models.CharField(max_length=60)
+    contact = models.CharField(max_length=40)
 
 # This is the Projects model
 class Projects(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
     title = models.CharField(max_length=30)
+    owner = models.ForeignKey(User, on_delete=CASCADE, related_name='projects')
     image = CloudinaryField('image')
-    projectdesc = models,CharField(max_length=150)
+    projectdesc = models,CharField(max_length=300)
     link = models.CharField(max_length=40)
-    ratings = models.CharField(max_length=30, default='0')
-    reviews = models.CharField(max_length=150, default='0')
+    date = models.DateField(auto_now_add=True, blank=True)
 
     # It will create the Project model and convert its inputs into a String
     def __str__(self):
@@ -31,11 +42,3 @@ class Projects(models.Model):
     # This is for updating the project in the app
     def update_project(self):
         self.update()
-
-# User Profile Model
-class Profile(models.Model):
-    id = models.CharField(primary_key=True, max_length=20)
-    image = CloudinaryField('image')
-    bio = models.CharField(max_length=40)
-    projects = models.CharField(max_length=60)
-    contact = models.CharField(max_length=40)
