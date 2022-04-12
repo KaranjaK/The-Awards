b108f4573e4d3f712dd95f97cd5aa27ffe06d3a8
KK <karanjakelvin77@gmail.com>
2022-04-12 03:47:57 +0300

    It will update the profile model

--- the_awards/models.py
+++ the_awards/models.py
@@ -1,27 +1,39 @@
-from pyexpat import model
-from tkinter import CASCADE
 from django.db import models
 from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
 from django.forms import CharField
 import datetime as dt
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Create your models here.
 
 # User Profile Model
 class Profile(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
-    owner = models.OneToOneField(User, on_delete=CASCADE, related_name='profile')
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     image = CloudinaryField('image')
     bio = models.CharField(max_length=40)
     projects = models.CharField(max_length=60)
-    contact = models.CharField(max_length=40)
+    contact = models.EmailField(max_length=150, blank=True)
+
+    def __str__(self):
+        return f'{self.owner.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(owner=instance)
+    
+    @receiver(post_save, sende=User)
+    def save_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 # This is the Projects model
 class Projects(models.Model):
     id = models.CharField(primary_key=True, max_length=20)
     title = models.CharField(max_length=30)
-    owner = models.ForeignKey(User, on_delete=CASCADE, related_name='projects')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='projects')
     image = CloudinaryField('image')
     projectdesc = models,CharField(max_length=300)
     link = models.CharField(max_length=40)
