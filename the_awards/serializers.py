69d6b8e143e9b0ab178589ef716a7bf71adaadfb
KK <karanjakelvin77@gmail.com>
2022-04-12 20:43:27 +0300

    It will add the serializers.py for API endpoints

---
+++ the_awards/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Profile, Projects
+from django.contrib.auth.models import User
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['id','user', 'image', 'bio', 'projects', 'contact']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ['id', 'title', 'title', 'user', 'image', 'projectdesc', 'link', 'date']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    profile = ProfileSerializer(read_only=True)
+    posts = PostSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'url', 'username', 'profile', 'projects']
