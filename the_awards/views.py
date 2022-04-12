d30c2b87975b6de7a6573021f0b7eb0bd988cdc6
KK <karanjakelvin77@gmail.com>
2022-04-12 19:39:26 +0300

    It will import the necessary imports to create views

--- the_awards/views.py
+++ the_awards/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import SignupForm, PostForm, UpdateUserForm, UpdateUserProfileForm, RatingsForm
+from rest_framework import viewsets
+from django.contrib.auth.decorators import login_required
+from .models import Profile, Post, Rating
+from .serializers import ProfileSerializer, UserSerializer, PostSerializer
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
+from django.http import HttpResponseRedirect
+import random
 
 # Create your views here.
